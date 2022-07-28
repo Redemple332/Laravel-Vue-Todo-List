3dfce0cff1331f7b8f6d9adbf48109c8ed78bcb5
Redemple32 <marceloredemple332@gmail.com>
2022-07-28 21:37:38 +0800

    first

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Item;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $items = Item::orderBy('created_at')->get();
+        return $items;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $item = Item::create([
+            'name' => $request->name
+        ]);
+
+        return $item;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $item = Item::find($id);
+
+        if($item){
+            $item->update([ 
+                'completed' => $item->completed ? true : false,
+                'completed_at' => $item->completed ? Carbon::now() : null
+            ]);
+            return $item;
+        }
+
+        return "Item not found.";
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $item = Item::find($id);
+        
+        if($item){
+            $item->delete();
+            return "Item Deleted!";
+        }else{
+            return "Item Not Found.";
+        }
+    }
+}
